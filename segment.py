6001bd1df637b4bbf21110841954263b0a908a26
Ali Farokh <s.alifarrokh@gmail.com>
2023-09-22 03:02:02 +0330

    implement algorithm

---
+++ segment.py
@@ -0,0 +1,187 @@
+"""
+Persian CTC-Segmentation of speech
+"""
+
+import argparse
+import os
+from math import ceil
+import warnings
+
+import numpy as np
+from scipy.special import softmax
+from tqdm import tqdm
+import librosa
+import torch
+import torchaudio
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
+from datasets.utils.logging import disable_progress_bar
+import ctc_segmentation as cs
+
+from utils import get_vocab, read_csv, read_lines, normalize_text
+from utils import write_output, determine_utterance_segments
+
+
+# Disable warnings
+warnings.filterwarnings('ignore')
+disable_progress_bar()
+
+# Argparse
+parser = argparse.ArgumentParser(description='Persian CTC Segmentation')
+parser.add_argument('--output_dir', default='output', type=str, help='Path to output directory')
+parser.add_argument('--metadata', required=True, type=str, help='Path to metadata (csv) file')
+parser.add_argument(
+    '--window_len',
+    default=8000,
+    type=int,
+    help='Window size for CTC Segmentation algorithm'
+)
+parser.add_argument('--device', default='cpu', type=str, help='Device (cpu/cuda)')
+parser.add_argument(
+    '--threshold',
+    default=-2,
+    type=float,
+    help='Segment validation threshold (Sum of log probabilities)'
+)
+parser.add_argument(
+    '--max_chunk_seconds',
+    default=20,
+    type=float,
+    help='Maximum duration of audio chunks for inference.'
+)
+
+# Parse & validate args
+args = parser.parse_args()
+assert args.device in ["cuda", "cpu"], "Invalid value for device"
+assert not os.path.exists(args.output_dir), "Output directory already exists"
+
+# load model
+vocabulary = get_vocab()
+processor = Wav2Vec2Processor.from_pretrained("./")
+model = Wav2Vec2ForCTC.from_pretrained("./").to(args.device)
+
+# Model-related helper functions
+def output_frames(n_features):
+    """
+    Calculates the numbber of output frames based on input signal length
+    """
+    return model._get_feat_extract_output_lengths(n_features)
+
+
+# Load metadata
+data = read_csv(args.metadata)
+
+# Create output directory
+os.mkdir(args.output_dir)
+res_transcript = os.path.join(args.output_dir, 'result.csv')
+
+# Process each audio
+print('Segmenting audio files ...')
+next_audio_number = 1
+for item in tqdm(data):
+
+    # Load the speech file
+    original_speech_array, original_sample_rate = torchaudio.load(item['audio_path'])
+    item['index_duration'] = original_speech_array.shape[1] / original_sample_rate
+    original_speech_array = original_speech_array.squeeze().numpy()
+
+    # Preprocess the speech file
+    sample_rate = processor.feature_extractor.sampling_rate
+    speech_array = librosa.resample(
+        np.asarray(original_speech_array),
+        orig_sr=original_sample_rate,
+        target_sr=sample_rate
+    )
+
+    # Feature extraction
+    features = processor(
+        speech_array,
+        sampling_rate=sample_rate,
+        return_tensors="pt",
+        padding=True
+    )
+    input_values = features.input_values.to(args.device)
+
+    # Create speech chunks
+    n_all_samples = input_values.shape[1]
+    samples_per_chunk = int(args.max_chunk_seconds * sample_rate)
+    border_samples = int(samples_per_chunk / 6)
+    middle_samples = samples_per_chunk - (2 * border_samples)
+    n_chunks = ceil(n_all_samples / middle_samples)
+
+    # ASR inference
+    final_logits = None
+    for i in range(n_chunks):
+        chunk_start = i * middle_samples
+        chunk_end = chunk_start + samples_per_chunk
+        last_chunk = False
+        if i == n_chunks - 1:
+            last_chunk = True
+        elif i == n_chunks - 2:
+            if (input_values.shape[1] - 1) - chunk_end <= 2 * border_samples:
+                chunk_end = input_values.shape[1]
+                last_chunk = True
+        chunk = input_values[:, chunk_start:chunk_end]
+
+        with torch.no_grad():
+            logits = model(chunk, attention_mask=torch.ones_like(chunk)).logits
+            if args.device == 'cuda':
+                logits = logits.cpu()
+            logits = logits.numpy().reshape((-1, len(vocabulary)))
+
+        left_border = 0 if i == 0 else border_samples
+        right_border = 0 if last_chunk else border_samples
+        start_index =  output_frames(left_border) if i > 0 else 0
+        end_index = logits.shape[0] - (output_frames(right_border) if not last_chunk else 0)
+        logits = logits[start_index:end_index, :]
+
+        if final_logits is None:
+            final_logits = logits
+        else:
+            final_logits = np.concatenate([final_logits, logits], axis=0)
+
+        if last_chunk:
+            break
+
+    # Compute log probabilities
+    log_probs = np.log(softmax(final_logits, axis=1))
+    item['log_probs'] = log_probs
+    item['index_duration'] = item['index_duration'] / log_probs.shape[0]
+
+    # Load transcripts
+    item['text'] = read_lines(item['transcript_path'])
+    item['text_normal'] = normalize_text(item['text'])
+
+    # CTC Segmentation config
+    config = cs.CtcSegmentationParameters()
+    config.char_list = vocabulary
+    config.min_window_size = args.window_len
+    config.index_duration = item['index_duration']
+    config.excluded_characters = ".,-?!:»«;'›‹()"
+    config.blank = vocabulary.index("|")
+    ground_truth_mat, utt_begin_indices = cs.prepare_text(config, item['text_normal'])
+    config.blank = 0
+
+    # Run CTC Segmentation
+    ctc_seg_result = cs.ctc_segmentation(config, item['log_probs'], ground_truth_mat)
+    timings, char_probs, char_list = ctc_seg_result
+    segments = determine_utterance_segments(
+        config,
+        utt_begin_indices,
+        char_probs,
+        timings,
+        item['text_normal'],
+        char_list
+    )
+
+    # Write outputs
+    next_audio_number = write_output(
+        res_transcript,
+        next_audio_number,
+        args.output_dir,
+        item['audio_path'],
+        original_speech_array,
+        original_sample_rate,
+        segments,
+        item['text'],
+        args.threshold,
+    )
---
+++ utils.py
@@ -0,0 +1,132 @@
+import json
+import math
+import os
+import numpy as np
+import soundfile as sf
+
+
+def read_lines(file_path):
+    with open(file_path) as f:
+        lines = f.readlines()
+        lines = [l.strip() for l in lines if l.strip()]
+    return lines
+
+
+def read_csv(csv_path, delimiter=','):
+    lines = read_lines(csv_path)
+    lines = list(map(lambda l: list(map(str.strip, l.split(delimiter))), lines))
+    keys = lines[0]
+    lines = lines[1:]
+    res = []
+    for line in lines:
+        res.append({keys[i]:line[i] for i in range(len(keys))})
+    return res
+
+
+def get_vocab():
+    vocab = json.load(open('vocab.json'))
+    vocab = sorted(list(vocab.items()), key=lambda x: x[1])
+    vocab = list(map(lambda x: x[0], vocab))
+    return vocab
+
+
+def normalize_text(transcript):
+    vocab = get_vocab()
+    lines = map(lambda l: l.replace(' ', '|'), transcript)
+    lines = map(lambda l: ''.join([c for c in l if c in vocab]).strip(), lines)
+    lines = list(lines)
+    return lines
+
+
+def _compute_time(index, align_type, timings):
+    """Compute start and end time of utterance.
+    Adapted from https://github.com/lumaku/ctc-segmentation
+
+    Args:
+        index:  frame index value
+        align_type:  one of ["begin", "end"]
+
+    Return:
+        start/end time of utterance in seconds
+    """
+    middle = (timings[index] + timings[index - 1]) / 2
+    if align_type == "begin":
+        return max(timings[index + 1] - 0.5, middle)
+    elif align_type == "end":
+        return min(timings[index - 1] + 0.5, middle)
+
+
+def determine_utterance_segments(config, utt_begin_indices, char_probs, timings, text, char_list):
+    """Utterance-wise alignments from char-wise alignments.
+    Adapted from https://github.com/lumaku/ctc-segmentation
+
+    Args:
+        config: an instance of CtcSegmentationParameters
+        utt_begin_indices: list of time indices of utterance start
+        char_probs:  character positioned probabilities obtained from backtracking
+        timings: mapping of time indices to seconds
+        text: list of utterances
+    Return:
+        segments, a list of: utterance start and end [s], and its confidence score
+    """
+    segments = []
+    min_prob = np.float64(-10000000000.0)
+    for i in range(len(text)):
+        start = _compute_time(utt_begin_indices[i], "begin", timings)
+        end = _compute_time(utt_begin_indices[i + 1], "end", timings)
+
+        start_t = start / config.index_duration_in_seconds
+        start_t_floor = math.floor(start_t)
+
+        # look for the left most blank symbol and split in the middle to fix start utterance segmentation
+        if char_list[start_t_floor] == config.char_list[config.blank]:
+            start_blank = None
+            j = start_t_floor - 1
+            while char_list[j] == config.char_list[config.blank] and j > start_t_floor - 20:
+                start_blank = j
+                j -= 1
+            if start_blank:
+                start_t = int(round(start_blank + (start_t_floor - start_blank) / 2))
+            else:
+                start_t = start_t_floor
+            start = start_t * config.index_duration_in_seconds
+
+        else:
+            start_t = int(round(start_t))
+
+        end_t = int(round(end / config.index_duration_in_seconds))
+
+        # Compute confidence score by using the min mean probability after splitting into segments of L frames
+        n = config.score_min_mean_over_L
+        if end_t <= start_t:
+            min_avg = min_prob
+        elif end_t - start_t <= n:
+            min_avg = char_probs[start_t:end_t].mean()
+        else:
+            min_avg = np.float64(0.0)
+            for t in range(start_t, end_t - n):
+                min_avg = min(min_avg, char_probs[t : t + n].mean())
+        segments.append((start, end, min_avg))
+    return segments
+
+
+def write_output(output, next_audio_number, output_dir, audio_path, speech_signal, sr, segments, text, th, delimiter='$'):
+    d = f' {delimiter} '
+    
+    if not os.path.exists(output):
+        with open(output, 'w', encoding='utf-8') as outfile:
+            outfile.write(f'original_audio_path{d}start{d}end{d}score{d}audio_path{d}text\n')
+    
+    with open(output, 'a', encoding='utf-8') as outfile:
+        for i, segment in enumerate(segments):
+            start, end, score = segment
+            audio_name = f'{next_audio_number}.wav'
+            segment_audio_path = os.path.join(output_dir, audio_name) if score > th else 'INVALID'
+            outfile.write(f'{audio_path}{d}{start}{d}{end}{d}{score}{d}{segment_audio_path}{d}{text[i]}\n')
+            if score > th:
+                start_index, end_index = int(start * sr), int(end * sr)
+                segment_signal = speech_signal[start_index:end_index]
+                sf.write(segment_audio_path, segment_signal, sr, subtype='PCM_24')
+                next_audio_number += 1
+    
+    return next_audio_number
